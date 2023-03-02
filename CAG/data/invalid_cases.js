af8f5da86d0c3bfd3431dfa42c5a04d8f190deac
lexuanbinh <binh.le.xuan@gmail.com>
2023-03-02 10:05:30 +0700

    update new data

--- CAG/data/invalid_cases.js
+++ CAG/data/invalid_cases.js
@@ -1,6 +1,9 @@
     let removed_ids = `
     [
-      {"removed_id": "NONE"},
+      {"removed_id": "f9e75abf-b44f-4788-bf1f-29c0812652d4"},
+	  {"removed_id": "5c70a718-fb02-4718-9be0-2119d37197f9"},
+	  {"removed_id": "9cbbd04a-7516-48f6-8f86-d023751aaf75"}
+	  
     ]
      
     `;
