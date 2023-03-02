c2cfe9041924e499c1eafd7dab666315cb43d701
lexuanbinh <binh.le.xuan@gmail.com>
2023-03-02 09:19:53 +0700

    update new data

--- CAG/data/invalid_cases.js
+++ CAG/data/invalid_cases.js
@@ -1,28 +1,6 @@
     let removed_ids = `
     [
-      {"removed_id": "502a6a1d-371f-4330-9883-b48bfe150bd5"},
-		{"removed_id": "1b1d5f43-bd16-4d4d-84a0-145d2091996c"},
-		{"removed_id": "5d4760ac-0126-49e3-97b2-0270c1354586"},
-		{"removed_id": "6ea041ba-3785-4d6d-8d9e-c1ff9bb1cab7"},
-		{"removed_id": "1f75c6ee-47ee-47fe-a450-32b40ba1a83b"},
-		{"removed_id": "d8cccd79-c954-4e8f-bc38-0409ecf218b4"},
-		{"removed_id": "5f46bd65-f637-4d41-a5c1-ea4605f4ceae"},
-		{"removed_id": "a682996b-5af0-46c0-b539-2500ad6f9f10"},
-		{"removed_id": "9812e8b4-cb69-430a-a228-bf41a8e471d2"},
-		{"removed_id": "7a7646e9-c258-41c7-9bfe-09f428ffa79a"},
-		{"removed_id": "49f11930-c414-4f4b-9c3e-bc9a65ea3f14"},
-		{"removed_id": "c259751e-7b5a-4fd5-b223-abf16a5956a7"},
-		{"removed_id": "bec65a55-e518-4995-b9ce-1c80e327b8e7"},
-		{"removed_id": "91f32c06-224b-4285-99c3-c5a273d581cd"},
-		{"removed_id": "e4e2896c-ee53-4702-bbfc-e484482c77bd"},
-		{"removed_id": "17eb3f7c-4da5-4e59-87e6-7c5f193dbd6a"},
-		{"removed_id": "4f4d6474-90a8-4f26-9bce-5b0363ac8451"},
-		{"removed_id": "47bb0f55-9768-4fa1-9ecb-23c2ad0f4411"},
-		{"removed_id": "29b64751-a323-4b37-835d-514a3154440d"},
-		{"removed_id": "04b2a7f6-f470-4ee8-8ffe-a750cc071c56"},
-		{"removed_id": "aaab83a0-6488-4532-b255-4bfb19884802"},
-		{"removed_id": "f9e75abf-b44f-4788-bf1f-29c0812652d4"}
-
+      {"removed_id": "NONE"},
     ]
      
     `;
